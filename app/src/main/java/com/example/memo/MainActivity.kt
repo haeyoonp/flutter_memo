371d90e615c9c4280075ca4a988d2e9ec3305c5e
hy04040 <qgpdbs44@gmail.com>
2021-02-05 12:10:32 +0900

    toobars and menus made

--- app/src/main/java/com/example/memo/MainActivity.kt
+++ app/src/main/java/com/example/memo/MainActivity.kt
@@ -1,22 +1,59 @@
-package com.example.memo;
+package com.example.memo
 
 import android.os.Bundle
 import android.view.Menu
+import android.view.MenuInflater
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
-import res.menu.*
+import androidx.appcompat.widget.Toolbar
 
 
 class MainActivity : AppCompatActivity() {
+
+    private var mTopToolbar: Toolbar? = null
+    private var mBottomToolbar: Toolbar? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        //setSupportActionBar(findViewById(R.id.main_toolbar))
+
+        mTopToolbar = findViewById(R.id.top_toolbar)
+        setSupportActionBar(mTopToolbar)
+
+        mBottomToolbar = findViewById(R.id.bottom_toolbar)
+        setSupportActionBar(mBottomToolbar)
+        //mBottomToolbar.inflateMenu(R.menu.bottom_menu, menu)
 
     }
+
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.main_toolbar, menu)
+        //val inflater: MenuInflater = menuInflater
+        mTopToolbar?.inflateMenu(R.menu.top_menu)
+        mBottomToolbar?.inflateMenu(R.menu.bottom_menu)
+        //inflater.inflate(R.menu.top_menu, menu)
+        //inflater.inflate(R.menu.bottom_menu, menu)
+
         return true
     }
+    /*
+    override fun onOptionsItemSelected(item: MenuItem) = when (item.itemId) {
+        R.id.action_settings -> {
+            // User chose the "Settings" item, show the app settings UI...
+            true
+        }
+
+        R.id.action_favorite -> {
+            // User chose the "Favorite" action, mark the current item
+            // as a favorite...
+            true
+        }
+
+        else -> {
+            // If we got here, the user's action was not recognized.
+            // Invoke the superclass to handle it.
+            super.onOptionsItemSelected(item)
+        }
+    }*/
 }
